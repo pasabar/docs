ac3f938df3e0b47604b77ce7fddce74edc1e9416
fahad17 <fahadra96@gmail.com>
2023-10-30 18:06:27 +0700

    update swagger js url data

--- js/swaggerfunc.js
+++ js/swaggerfunc.js
@@ -1,6 +1,6 @@
 import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
 
-export let URLData = "https://raw.githubusercontent.com/pasabar/docs/main/yaml/openapi.yaml";
+export let URLData = "./yaml/openapi.yaml";
 
 export const UIData = SwaggerUIBundle({
     url: URLData, //Location of Open API spec in the repo
