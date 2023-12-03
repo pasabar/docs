c303c95df3957e35f199e6f8aae83fd4a646d844
fahad17 <fahadra96@gmail.com>
2023-12-03 18:39:09 +0700

    Fix import formatting and update URLData in swaggerfunc.js

--- js/swaggerfunc.js
+++ js/swaggerfunc.js
@@ -1,20 +1,19 @@
-import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
+import SwaggerUIBundle from "https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js";
 
-export let URLData = "https://raw.githubusercontent.com/pasabar/docs/main/yaml/openapi.yaml";
+export let URLData =
+  "https://raw.githubusercontent.com/pasabar/swagger/main/yaml/openapi.yaml";
 
 export const UIData = SwaggerUIBundle({
-    url: URLData, //Location of Open API spec in the repo
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIBundle.SwaggerUIStandalonePreset
-    ],
-    plugins: [
-        SwaggerUIBundle.plugins.DownloadUrl
-    ],
+  url: URLData, //Location of Open API spec in the repo
+  dom_id: "#swagger-ui",
+  deepLinking: true,
+  presets: [
+    SwaggerUIBundle.presets.apis,
+    SwaggerUIBundle.SwaggerUIStandalonePreset,
+  ],
+  plugins: [SwaggerUIBundle.plugins.DownloadUrl],
 });
 
 export function setSwagger() {
-    return UIData
+  return UIData;
 }
