c38aea32b90c5d2082f0db47cf76601b21af49d7
ryaasishlah <ryaasishlah123@gmail.com>
2023-12-05 03:56:46 +0700

    update

--- js/swaggerfunc.js
+++ js/swaggerfunc.js
@@ -1,19 +1,34 @@
-import SwaggerUIBundle from "https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js";
+import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
 
-export let URLData =
-  "https://raw.githubusercontent.com/pasabar/swagger/main/yaml/openapi.yaml";
+export let URLData = "https://raw.githubusercontent.com/pasabar/swagger/main/yaml/openapi.yaml";
 
 export const UIData = SwaggerUIBundle({
-  url: URLData, //Location of Open API spec in the repo
-  dom_id: "#swagger-ui",
-  deepLinking: true,
-  presets: [
-    SwaggerUIBundle.presets.apis,
-    SwaggerUIBundle.SwaggerUIStandalonePreset,
-  ],
-  plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    url: URLData,
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+    ],
+    requestInterceptor: (request) => {
+        // Tambahkan logika untuk menyertakan token ke setiap permintaan
+        // Contoh: request.headers['Login'] = 'your-api-key';
+
+        // Ambil token API key dari tempat penyimpanan atau tempat lain yang sesuai
+        const apiKey = localStorage.getItem('api_key');
+
+        // Sisipkan token API key ke header permintaan jika tersedia
+        if (apiKey) {
+            request.headers['Login'] = apiKey;
+        }
+
+        return request;
+    },
+    plugins: [
+        SwaggerUIBundle.plugins.DownloadUrl
+    ],
 });
 
 export function setSwagger() {
-  return UIData;
+    return UIData;
 }
