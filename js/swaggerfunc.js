5d591dce53bdceb7cf4a6703233ea0b92b20a75b
ryaasishlah <ryaasishlah123@gmail.com>
2023-12-05 20:18:15 +0700

    fix update func dan update urldata

--- js/swaggerfunc.js
+++ js/swaggerfunc.js
@@ -1,34 +1,21 @@
 import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
+import SwaggerUIStandalonePreset from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-standalone-preset.js';
 
-export let URLData = "https://raw.githubusercontent.com/pasabar/swagger/main/yaml/openapi.yaml";
+export let URLData = "./yaml/openapi.yaml";
 
 export const UIData = SwaggerUIBundle({
-    url: URLData,
+    url: URLData, //Location of Open API spec in the repo
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
         SwaggerUIBundle.presets.apis,
-        SwaggerUIBundle.SwaggerUIStandalonePreset
+        SwaggerUIStandalonePreset
     ],
-    requestInterceptor: (request) => {
-        // Tambahkan logika untuk menyertakan token ke setiap permintaan
-        // Contoh: request.headers['Login'] = 'your-api-key';
-
-        // Ambil token API key dari tempat penyimpanan atau tempat lain yang sesuai
-        const apiKey = localStorage.getItem('api_key');
-
-        // Sisipkan token API key ke header permintaan jika tersedia
-        if (apiKey) {
-            request.headers['Login'] = apiKey;
-        }
-
-        return request;
-    },
     plugins: [
         SwaggerUIBundle.plugins.DownloadUrl
-    ],
+    ]
 });
 
 export function setSwagger() {
-    return UIData;
+    return UIData
 }
